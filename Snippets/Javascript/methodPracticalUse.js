50fe1736742d35f3ba1317a0130f98ae87f55bfe
Vaibhav Vyas <52570863+GenxTheGamer@users.noreply.github.com>
2021-10-07 18:11:40 +0530

    added methodPracticalUse.js

---
+++ Snippets/Javascript/methodPracticalUse.js
@@ -0,0 +1,9 @@
+const nameSpeak = {};
+
+nameSpeak.name = "My Name is Vaibhav"; 
+
+nameSpeak.speak = console.log(nameSpeak.name); //Basically Added Function inside an object.
+
+
+
+consol.log(nameSpeak); //This shows whats inside the object.
